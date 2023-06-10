5829ebcdfb34ef01712f46273c8ea62f1ccc3e9f
ArsenYeritsyan <yeritsyan.arsen@gmail.com>
2023-06-11 02:44:13 +0400

    fixing

--- src/main/java/com/crud/contacts/repo/ContactRepository.java
+++ src/main/java/com/crud/contacts/repo/ContactRepository.java
@@ -2,7 +2,6 @@ package com.crud.contacts.repo;
 
 import com.crud.contacts.model.Contact;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
